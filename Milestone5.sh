3b3db9eb92293cf05b1e93120f3ad3fb3fd02976
DertjeR <99654776+DertjeR@users.noreply.github.com>
2022-03-15 23:50:51 +0100

    Update Milestone5.sh

--- Milestone5.sh
+++ Milestone5.sh
@@ -2,6 +2,9 @@
 #This shell script contains code that downloads the wikipedia pagecount the word 'de' on the wikipedia page of Rijksuniversiteit Groningen
 #When you have downloaded the wikipedia as Rijksuniversiteit_groningen.txt you should put it in the same location as this file
 
+#Author: D.J. Roggeveen s3973972
+# version number of linux shell: 21.1.0
+
 #use the following command to make your file executable:
 #chmod +x Milestone5.sh
 #after that do the following:
