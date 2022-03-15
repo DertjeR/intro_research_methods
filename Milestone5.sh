082295a0a45b045e5e150474875eb580db0c8d86
DertjeR <99654776+DertjeR@users.noreply.github.com>
2022-03-15 23:52:00 +0100

    Update Milestone5.sh

--- Milestone5.sh
+++ Milestone5.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#This shell script contains code that downloads the wikipedia pagecount the word 'de' on the wikipedia page of Rijksuniversiteit Groningen
-#When you have downloaded the wikipedia as Rijksuniversiteit_groningen.txt you should put it in the same location as this file
+#This shell script contains code that downloads the wikipedia pagecount 
+#the word 'de' on the wikipedia page of Rijksuniversiteit Groningen
+#When you have downloaded the wikipedia as Rijksuniversiteit_groningen.txt 
+#you should put it in the same location as this file
 
 #Author: D.J. Roggeveen s3973972
 # version number of linux shell: 21.1.0
@@ -10,5 +12,6 @@
 #after that do the following:
 #Usage: ./Milestone5.sh Rijksuniversiteit_groningen.txt
 
-#The code below counts the number of occurences of the word 'de'. It counts only words, not part of strings and is case insensitive
+#The code below counts the number of occurences of the word 'de'. 
+#It counts only words, not part of strings and is case insensitive
 grep -o -i -w de Rijksuniversiteit_Groningen.txt | wc -l
