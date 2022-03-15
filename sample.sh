9190dd8bd408d91270b61abf3fd3aa58db2889b4
DertjeR <99654776+DertjeR@users.noreply.github.com>
2022-03-15 23:19:04 +0100

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #count the word 'de' on the wikipedia page of Rijksuniversiteit Groningen
+#wikipedia page downloaded as pdf on 15-03-2022 at 22:42 CET
+#results: 234
+
 sudo apt install poppler-utils
 pdftotext -layout Rijksuniversiteit_Groningen.pdf Rijksuniversiteit_Groningen.txt
 grep -o -i -w de Rijksuniversiteit_Groningen.txt | wc -l
